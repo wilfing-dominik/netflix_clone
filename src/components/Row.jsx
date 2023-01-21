a8fb2076987907b6b481971b14c7f8b0d7c94ea1
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-01-21 04:05:14 +0100

    added tv show rows

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../styles/Row.css";
+
+const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, url }) {
-  return <div className="row">{title}</div>;
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(url);
+      setShows(request.data.results);
+    }
+    fetchData();
+  }, [url]);
+
+  console.log(shows);
+
+  return (
+    <div className="row-posters">
+      {shows.map((show) => (
+        <img
+          className="row-poster"
+          src={imageBaseUrl + show.backdrop_path}
+          alt={show.name}
+        ></img>
+      ))}
+    </div>
+  );
 }
 
 export default Row;
