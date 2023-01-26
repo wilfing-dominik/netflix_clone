aeacbba5a756cbe9b1ade7234dccdd3efb8518a7
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-01-26 19:50:40 +0100

    fixed bad link nesting in browse menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,8 +57,8 @@ function Navbar() {
               }}
             >
               Browse
+              {isBrowseMenuClicked && <MainMenu customClass="browse-menu" />}
             </a>
-            {isBrowseMenuClicked && <MainMenu customClass="browse-menu" />}
           </>
         )}
       </div>
