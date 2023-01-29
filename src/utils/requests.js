da1f97df748f797bce32f33bfb4e9ed6fd21ecd8
wilfing-dominik <56601102+wilfing-dominik@users.noreply.github.com>
2023-01-29 22:37:54 +0100

    added "new shows" api based on current date

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,5 +1,7 @@
 const API_KEY = "772e434a2b577aeab0e8724d77d306ce";
 const baseUrl = "https://api.themoviedb.org/3/";
+export const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
+export let currentDate = new Date().toISOString().slice(0, 10);
 
 const requests = {
   nowTrending: baseUrl + `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -9,6 +11,16 @@ const requests = {
     baseUrl + `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   comedy: baseUrl + `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   horror: baseUrl + `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  arrivals:
+    baseUrl +
+    `/discover/tv?api_key=${API_KEY}&first_air_date.gte=${new Date(
+      Date.now() - 7 * 24 * 60 * 60 * 1000
+    )
+      .toISOString()
+      .slice(
+        0,
+        10
+      )}&first_air_date.lte=${currentDate}&language=en-US&region=US&sort_by=popularity.desc&page=1&with_origin_country=US`,
 };
 
 export default requests;
