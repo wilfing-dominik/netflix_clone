7796db4b81d018b2008b9436b1a2ec796b908ab8
wilfing-dominik <wilfing.dominik00@gmail.com>
2023-02-19 19:53:06 +0100

    added base for responsive search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../styles/SearchBar.css";
+import axios from "../utils/requests";
+import { useFetchData, useFilterData } from "../utils/customHooks";
 
 export default function SearchBar({ placeHolder, setIsSearchBarFocused }) {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  // useFilterData(axios.nowTrending, setResults, input);
+  useFetchData(axios.allTVShows, setResults);
+
   return (
-    <input
-      autoFocus
-      onBlur={() => {
-        setIsSearchBarFocused(false);
-      }}
-      className="search-input"
-      type="text"
-      name="search"
-      placeholder={placeHolder}
-    />
+    <>
+      <input
+        autoFocus
+        onBlur={() => {
+          setIsSearchBarFocused(false);
+        }}
+        className="search-input"
+        type="text"
+        name="search"
+        placeholder={placeHolder}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div className="search-results">
+        {results.map((result) => (
+          <div key={result.id}>
+            {result.name ? result.name : result.title} {results.length}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
